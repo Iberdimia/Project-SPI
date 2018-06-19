30e4e261d0d3d055203d5e28ea3b64569dfe7df9
Dimia IBERRAKEN <diiberrake@sciapp5.sciences.lan>
2018-06-19 15:02:25 +0200

    desc

---
+++ build/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/code.dir/demo.cpp.o"
+  "code.pdb"
+  "code"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/library.dir/student.cpp.o"
+  "liblibrary.pdb"
+  "liblibrary.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
